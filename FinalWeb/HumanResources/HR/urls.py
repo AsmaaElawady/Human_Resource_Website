afd7723f3c44562915f403dbd06bfdb9b441d82a
Mawada-22 <94462827+Mawada-22@users.noreply.github.com>
2023-05-22 15:18:41 +0300

    Update urls.py

--- FinalWeb/HumanResources/HR/urls.py
+++ FinalWeb/HumanResources/HR/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('update/updaterecord/<int:EmployeeID>', views.updaterecord, name='updaterecord'),
     path('searchEmployee/',views.searchEmployee,name = 'searchEmployee'),
     path('table',views.Table,name = "table"),
+    path('VR/',views.VRDisplay,name='VRDisplay'),
     # path('update/', views.update, name='update'),
     # path('updateEmp/', views.updateEmp, name='updateEmp'),
     # # path('delete/<event_id>', views.delete, name='delete'),
